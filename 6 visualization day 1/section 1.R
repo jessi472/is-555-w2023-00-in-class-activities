17c516917ac98a208206cfc900b0969dac89913b
jessi472 <79349080+jessi472@users.noreply.github.com>
2023-02-28 13:07:49 -0700

    Update section 1.R

--- 6 visualization day 1/section 1.R
+++ 6 visualization day 1/section 1.R
@@ -39,7 +39,23 @@
 #       Most relevant: identity, stack, dodge
 #   color <- defines the color of the elements produced by that geom
 #   alpha <- defines the opacity (or "see-through-ness") of the elements produced by that geom
-#   
+# 
+# FACETS
+#   Facets allow you to use categories within your data to line up similar
+#   charts next to each other, which can be a really useful way to highlight
+#   differences between groups.
+# 
+# The two main facet functions are:
+#   facet_wrap() <- usually best used to show a series of plots across a 
+#                  single category variable, with you specify with `~varable`.
+#                  This will show one plot per level of the category.
+#   facet_grid() <- My favorite to show a "grid" of facets, with rows and 
+#                  columns defined by two different variables. This is specified
+#                  using the facet formula `var1~var2`
+#       Note: Pay attention to the `scales` parameter in these facet functions, 
+#             where you can specify whether you want to hold the x and/or y
+#             axis scales constant across all of the facets.
+# 
 
 library(tidyverse)
 
@@ -104,6 +120,45 @@ penguins %>%
 
 
 # versus: mapping the color specific to the scatter points only
+
+
+# Facets and multi-lines --------------------------------------------------------------------------------
+
+tips <- read_csv('https://www.dropbox.com/s/rydxlxdarjdoj7a/tips.csv?dl=1')
+
+# Let's plot tip_percentage vs. total_bill,
+# then split that across lots of categories
+tips %>% 
+  #mutate(tip_perc = tip/total_bill) %>% 
+  ggplot(aes(x = total_bill, y = tip, color = sex, shape = smoker)) +
+  geom_point(size = 2)+
+  facet_wrap(smoker~size, nrow =2)# or vars(day) scales help with skewed data
+#scales - fixed free, or free_x or free_y
+
+tips %>% 
+  mutate(tip_perc = tip/total_bill) %>% 
+  ggplot(aes(x = total_bill, y = tip_perc, color = as.factor(size), shape = smoker)) +
+  geom_point(size = 2)+
+  facet_grid(smoker~day)# set number of tables and where located 
+#wrap - doesn't show empty
+
+econ <- read_csv('https://www.dropbox.com/s/8bq9rw0rk46hru2/econ.csv?dl=1')
+
+# Let's plot two measures over time: savings rate & unemployment weeks
+# It's easiest if we pivot to make this work
+econ %>% 
+  select(date, savings_rate, unempl_weeks) %>%
+  #filter(date < lubridate::mdy('01-01-1970')) %>% 
+  pivot_longer(
+    savings_rate:unempl_weeks,
+    names_to = 'Measure',
+    values_to = 'Rate'
+  ) %>% 
+  ggplot(aes(x = date, y = Rate, color = Measure))+
+  geom_line(size = 1.5)#+
+  #facet_wrap(~Measure, nrow = 2)
+
+#tibble$col - get min, max, etc
 
 
 
